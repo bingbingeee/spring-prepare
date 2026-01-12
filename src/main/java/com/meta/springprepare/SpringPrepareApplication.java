f82daa136c6feebee12d82eca1a593f35bb1a050
bingbingeee <binnyyoonbin@gmail.com>
2026-01-12 10:47:40 +0900

    docs: apllicationproperties 서버 설정, 병경 테스트

--- src/main/java/com/meta/springprepare/SpringPrepareApplication.java
+++ src/main/java/com/meta/springprepare/SpringPrepareApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringPrepareApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringPrepareApplication.class, args);
+        System.out.println("Hello");
     }
 
 }
